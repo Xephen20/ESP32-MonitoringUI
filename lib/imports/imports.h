5833491af119f521dae1b39a42bbc6b406a95252
Dawid Szewc <xephen20@gmail.com>
2022-12-02 11:46:54 +0100

    camera

---
+++ lib/imports/imports.h
@@ -0,0 +1,9 @@
+#include "esp_camera.h"
+#include <WiFi.h>
+#include "esp_timer.h"
+#include "img_converters.h"
+#include "Arduino.h"
+#include "fb_gfx.h"
+#include "soc/soc.h" //disable brownout problems
+#include "soc/rtc_cntl_reg.h"  //disable brownout problems
+#include "esp_http_server.h"
